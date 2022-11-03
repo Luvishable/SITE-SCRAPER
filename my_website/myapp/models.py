f5ed56f13e1fa8d93857673df761bf1cb81e6d98
AHMET <ahmetfaruklacin48@gmail.com>
2022-11-03 12:40:19 +0300

    Adding bootstrap, making the scraping dynamic, creating delete view

--- my_website/myapp/models.py
+++ my_website/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    # sometimes certain links might not have a proper address and we need to allow those addresses to be null
+    address = models.CharField(max_length=1000, null=True, blank=True)
+    name = models.CharField(max_length=1000, null=True, blank=True)
+
+    # After creating the Link model, you have to also make sure that your view - in this case scrape- saves address and
+    # name of the links to the database
+
--- my_website/myapp/views.py
+++ my_website/myapp/views.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
+from .models import Link
 
 # Create your views here.
 
 def scrape(request):
 
-    # Getting the page
-    page = requests.get("https://www.google.com")
+    if request.method == "POST":
+        # remember! the name of the 'id' and the 'name' attributes in the input field is site!!
+        site = request.POST.get("site", "")
 
-    # Getting the html of the page
-    soup = BeautifulSoup(page.text, "html.parser")
+        # Getting the page
+        page = requests.get("https://www.google.com")
 
-    link_addresses = []
-    for link in soup.find_all("a"):
+        # Getting the html of the page
+        soup = BeautifulSoup(page.text, "html.parser")
 
-        # Adding the contents of the href attributes into the list
-        link_addresses.append(link.get("href"))
+        for link in soup.find_all("a"):
+            link_address = link.get("href")
+            link_text = link.string
+            Link.objects.create(address=link_address, name=link_text)
+
+        return HttpResponseRedirect("/")
+
+    else:
+        data = Link.objects.all()
 
     # Render the result.html and give the html context which is the last parameter of the render method
-    return render(request, "myapp/result.html", {"link_addresses": link_addresses})
+    return render(request, "myapp/result.html", {"data": data})
 
+def clear(request):
+    Link.objects.all().delete()
+    return render(request, "myapp/result.html")
 
 
 
